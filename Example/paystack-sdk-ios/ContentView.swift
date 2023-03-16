36f00f149a89b5be2e50979db2a570abe9b32c38
Zaheer <zaheer-dvt@paystack.com>
2023-03-16 13:17:11 +0200

    Removed UIKit example app and replaced it with a SwiftUI version

---
+++ Example/paystack-sdk-ios/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+import Paystack
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Paystack SDK Example App")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
