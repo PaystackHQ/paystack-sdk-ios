312f695523bbaf7b8e5da590548a9c01a5b85cd6
Zaheer <zaheer-dvt@paystack.com>
2023-06-19 17:31:24 +0200

    Swiftlint cleanup

--- Sources/PaystackUI/Components/ViewTask.swift
+++ Sources/PaystackUI/Components/ViewTask.swift
@@ -6,7 +6,8 @@
 
 import SwiftUI
 
-@available(iOS, deprecated: 15.0, message: "AsyncCompatibilityKit is only useful when targeting iOS versions earlier than 15")
+@available(iOS, deprecated: 15.0,
+           message: "AsyncCompatibilityKit is only useful when targeting iOS versions earlier than 15")
 public extension View {
     /// Attach an async task to this view, which will be performed
     /// when the view first appears, and cancelled if the view
--- Tests/PaystackSDKTests/API/Transactions/TransactionsTests.swift
+++ Tests/PaystackSDKTests/API/Transactions/TransactionsTests.swift
@@ -2,18 +2,18 @@ import XCTest
 @testable import PaystackCore
 
 class TransactionsTests: PSTestCase {
-    
+
     let apiKey = "testsk_Example"
-    
+
     var serviceUnderTest: Paystack!
-    
+
     override func setUpWithError() throws {
         try super.setUpWithError()
         serviceUnderTest = try PaystackBuilder.newInstance
             .setKey(apiKey)
             .build()
     }
-    
+
     func testVerifyAccessCode() async throws {
         mockServiceExecutor
             .expectURL("https://api.paystack.co/transaction/verify_access_code/access_code_test")
@@ -23,5 +23,5 @@ class TransactionsTests: PSTestCase {
 
         _ = try await serviceUnderTest.verifyAccessCode("access_code_test").async()
     }
-    
+
 }
