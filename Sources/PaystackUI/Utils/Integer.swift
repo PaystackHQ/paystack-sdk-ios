75dfacf2580d9cb35a596e1350ebb3bb562dea1d
Zaheer <zaheer-dvt@paystack.com>
2023-07-24 17:56:52 +0200

    Fixed swiftlint complaint

--- Sources/PaystackUI/Utils/Integer.swift
+++ Sources/PaystackUI/Utils/Integer.swift
@@ -6,7 +6,7 @@ extension Int {
         guard self >= 0 else { return "00:00" }
         let minutes = self / 60
         let seconds = self % 60
-        return String(format:"%02d:%02d", minutes, seconds)
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 
 }
