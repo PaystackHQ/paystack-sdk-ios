4fefb2341648bec521097ba8b9175b5cdfe81921
Zaheer <zaheer-dvt@paystack.com>
2023-08-28 23:53:14 +0200

    Added logic (and tests) for ChargeCardRepository to hit the actual Paystack object endpoints

--- Sources/PaystackUI/Charge/ChargeCard/Repository/ChargeCardRepositoryImplementation.swift
+++ Sources/PaystackUI/Charge/ChargeCard/Repository/ChargeCardRepositoryImplementation.swift
@@ -1,25 +1,40 @@
 import PaystackCore
 
-// TODO: Make actual calls here once we finalize API mocking
 struct ChargeCardRepositoryImplementation: ChargeCardRepository {
 
+    let paystack: Paystack
+
+    init() {
+        self.paystack = PaystackContainer.instance.retrieve()
+    }
+
     func submitBirthday(_ birthday: String, accessCode: String) async throws -> ChargeCardTransaction {
-        ChargeCardTransaction.example
+        let response = try await paystack.authenticateCharge(withBirthday: birthday,
+                                                             accessCode: accessCode).async()
+        return ChargeCardTransaction.from(response)
     }
 
     func submitPhone(_ phone: String, accessCode: String) async throws -> ChargeCardTransaction {
-        ChargeCardTransaction.example
+        let response = try await paystack.authenticateCharge(withPhone: phone,
+                                                             accessCode: accessCode).async()
+        return ChargeCardTransaction.from(response)
     }
 
     func submitOTP(_ otp: String, accessCode: String) async throws -> ChargeCardTransaction {
-        ChargeCardTransaction.example
+        let response = try await paystack.authenticateCharge(withOtp: otp,
+                                                             accessCode: accessCode).async()
+        return ChargeCardTransaction.from(response)
     }
 
     func submitAddress(_ address: Address, accessCode: String) async throws -> ChargeCardTransaction {
-        ChargeCardTransaction.example
+        let response = try await paystack.authenticateCharge(withAddress: address,
+                                                             accessCode: accessCode).async()
+        return ChargeCardTransaction.from(response)
     }
 
     func submitPin(_ pin: String, accessCode: String) async throws -> ChargeCardTransaction {
-        ChargeCardTransaction.example
+        let response = try await paystack.authenticateCharge(withPin: pin,
+                                                             accessCode: accessCode).async()
+        return ChargeCardTransaction.from(response)
     }
 }
