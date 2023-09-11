febafcf358d8b4701baa93fc2bd9be71f1a75036
Zaheer <zaheer-dvt@paystack.com>
2023-09-11 15:02:57 +0200

    Fixed a few swiftlint issues

--- UIKit Example/UIKit Example/SceneDelegate.swift
+++ UIKit Example/UIKit Example/SceneDelegate.swift
@@ -4,8 +4,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 }
-
