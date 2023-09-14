7314d6aab5f96c7c1b8c976ad90d72406b437704
Zaheer <zaheer-dvt@paystack.com>
2023-09-14 19:49:12 +0200

    Undoing model changes

--- Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
+++ Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
@@ -1,9 +1,8 @@
 import Foundation
 
-// TODO: Update this model once the responses are aligned with the contracts
 public struct Authorization: Codable {
     public var authorizationCode, bin, last4: String?
-    public var expMonth, expYear, reusable: Int?
+    public var expMonth, expYear, reusable: String?
     public var channel, cardType, bank: String?
     public var countryCode, brand: String?
     public var signature, accountName: String?
