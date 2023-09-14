57e83b7c4d15b357ac8f9b02edfec62aecce454a
Zaheer <zaheer-dvt@paystack.com>
2023-09-14 14:14:54 +0200

    Aligned models for charge response with actual response from endpoint

--- Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
+++ Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
@@ -1,9 +1,10 @@
 import Foundation
 
+// TODO: Update this model once the responses are aligned with the contracts
 public struct Authorization: Codable {
-    public var authorizationCode, bin, last4, expMonth: String
-    public var expYear, channel, cardType, bank: String
-    public var countryCode, brand: String
-    public var reusable: Bool
+    public var authorizationCode, bin, last4: String?
+    public var expMonth, expYear, reusable: Int?
+    public var channel, cardType, bank: String?
+    public var countryCode, brand: String?
     public var signature, accountName: String?
 }
--- Sources/PaystackSDK/Core/Models/Models/Charge/ChargeResponseData.swift
+++ Sources/PaystackSDK/Core/Models/Models/Charge/ChargeResponseData.swift
@@ -4,19 +4,20 @@ public struct ChargeResponseData: Codable {
     public var status: TransactionStatus
     public var amount: Double
     public var currency: String
-    public var transactionDate: String
-    public var reference: String
+    public var transactionDate: String?
+    public var reference: String?
     public var domain: Domain
     public var redirectUrl: String?
     public var url: String?
     public var ussdCode: String?
     public var qrCode: String?
     public var displayText: String?
-    public var metadata: [String: String]?
+    // TODO: Update this field once the responses are aligned with the contracts
+    public var metadata: String?
     public var gatewayResponse: String?
     public var message: String?
-    public var channel: String
-    public var fees: Double
+    public var channel: String?
+    public var fees: Double?
     public var authorization: Authorization?
     public var customer: Customer?
 }
