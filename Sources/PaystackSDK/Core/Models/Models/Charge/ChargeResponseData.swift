4ffae9005a42ae8f0fb79805dd0baa098cd36315
Zaheer <112851169+zaheer-dvt@users.noreply.github.com>
2023-09-14 20:27:53 +0200

    Undoing model changes (#87)
    * Undoing model changes
    
    * Fixed mistaken reusable value

--- Sources/PaystackSDK/Core/Models/Models/Charge/ChargeResponseData.swift
+++ Sources/PaystackSDK/Core/Models/Models/Charge/ChargeResponseData.swift
@@ -12,8 +12,7 @@ public struct ChargeResponseData: Codable {
     public var ussdCode: String?
     public var qrCode: String?
     public var displayText: String?
-    // TODO: Update this field once the responses are aligned with the contracts
-    public var metadata: String?
+    public var metadata: [String: String]?
     public var gatewayResponse: String?
     public var message: String?
     public var channel: String?
