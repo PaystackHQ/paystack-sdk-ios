b8df6dfe3abc553d53ecc3a03f5418c5956fa0b7
Zaheer <zaheer-dvt@paystack.com>
2023-09-14 19:56:02 +0200

    Fixed mistaken reusable value

--- Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
+++ Sources/PaystackSDK/Core/Models/Models/Charge/Authorization.swift
@@ -2,8 +2,9 @@ import Foundation
 
 public struct Authorization: Codable {
     public var authorizationCode, bin, last4: String?
-    public var expMonth, expYear, reusable: String?
+    public var expMonth, expYear: String?
     public var channel, cardType, bank: String?
     public var countryCode, brand: String?
+    public var reusable: Bool?
     public var signature, accountName: String?
 }
