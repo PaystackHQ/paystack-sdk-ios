d75f2055cf09112e58e97006334d71204ea13ffb
Peter-John <82087393+Peter-John-paystack@users.noreply.github.com>
2024-05-22 16:53:16 +0200

    MOB-934 Update Read Me and CodeOwners (#113)
    - Updated Readme with sample of Result Transaction
    	- Adding Code Owner file
    	- Updated the Example application for Result Tranaction

--- Example/paystack-sdk-ios/ContentView.swift
+++ Example/paystack-sdk-ios/ContentView.swift
@@ -21,7 +21,16 @@ struct ContentView: View {
         .padding()
     }
 
-    func paymentDone(_ result: TransactionResult) {}
+    func paymentDone(_ result: TransactionResult) {
+        switch result {
+        case .completed(let details):
+            print("Transaction completed with reference: \(details.reference)")
+        case .cancelled:
+            print("Transaction was cancelled")
+        case .error(error: let error, reference: let reference):
+            print("An error occured: \(error.message) with reference: \(String(describing: reference))")
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
